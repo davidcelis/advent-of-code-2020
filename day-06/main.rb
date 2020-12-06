001e408266c8b9a6e8ac51a1a74b2ec1886e4b15
David Celis <me@davidcel.is>
2020-12-06 09:31:58 -0800

    Add solution for Day 06, part two

--- day-06/main.rb
+++ day-06/main.rb
@@ -1,10 +1,16 @@
+require 'set'
+
 class SurveyGroup
   def initialize(group)
-    @text = group
+    @surveys = group.split("\n").map { |survey| survey.split('').to_set }
   end
 
-  def all_questions_answered
-    @text.gsub(/\s+/, '').split('').uniq
+  def questions_answered_by_anyone
+    @surveys.reduce(&:union)
+  end
+
+  def questions_answered_by_everyone
+    @surveys.reduce(&:intersection)
   end
 end
 
@@ -15,8 +21,17 @@ survey_groups = batch.map { |b| SurveyGroup.new(b) }
 # Part One
 
 sum = survey_groups.
-  map(&:all_questions_answered).
+  map(&:questions_answered_by_anyone).
   map(&:count).
   reduce(&:+)
 
 puts "The sum of unique questions answered per group was #{sum}"
+
+# Part Two
+
+sum = survey_groups.
+  map(&:questions_answered_by_everyone).
+  map(&:count).
+  reduce(&:+)
+
+puts "The sum of questions per group that were answered by everyone in that group was #{sum}"
