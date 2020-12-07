7dfd6d00bc451c811b5e5598d733a202c8a141ba
David Celis <me@davidcel.is>
2020-12-07 11:46:09 -0800

    Adjust solution for Day 07 to use a graph

--- day-07/main.rb
+++ day-07/main.rb
@@ -1,46 +1,103 @@
+# In the context of our graph, a Node represents a single bag.
+class Node
+  # The color of this bag, usually two words (e.g. posh black)
+  attr_reader :color
+
+  # An Array of Edges representing which bags (and how many of each) can
+  # be held by this bag.
+  attr_reader :edges
+
+  def initialize(color)
+    @color = color
+    @edges = []
+  end
+end
+
+# In the context of our graph, an Edge represents a directional connection
+# between two bags (nodes). An edge is directional because one bag will contain
+# a specific number of other bags.
+class Edge
+  # A bag that can contain other bags
+  attr_reader :container
+
+  # A bag that can be held by the container
+  attr_reader :content
+
+  # The number of bags that can be held by the container
+  attr_reader :amount
+
+  def initialize(container:, content:, amount:)
+    @container = container
+    @content = content
+    @amount = amount
+  end
+end
+
 class BagGraph
+  BAG_CONTENT_SCANNER = /(?:(\d+? ?\w+ \w+) bag)/
+
   def initialize
-    @graph = Hash.new { |h,k| h[k] = [] }
+    @nodes = []
+    @edges = []
   end
 
-  # This very basic "graph" pretty much just tracks: given a color (the key) of
-  # bag, many other colors (the values) can hold this bag? The graph's
-  # connections are kinda reversed, but it's to help us find our answer.
-  def register(container, colors)
-    colors.each do |color|
-      @graph[color] << container
-    end
+  # A shortcut to pull a Bag from the graph without having to navigate edges.
+  def find(color)
+    @nodes.find { |b| b.color == color }
   end
 
-  def containers_for(color)
-    all_containers = @graph[color]
+  # Adds a new bag to the graph and returns it
+  def add(color)
+    Node.new(color).tap { |node| @nodes << node }
+  end
 
-    @graph[color].each do |container|
-      all_containers += containers_for(container)
+  # Returns a Set of unique containers that can hold a given bag, whether
+  # directly or by holding a bag that can, itself, hold the given bag.
+  #
+  # recursion: node => edges(node) => containers => edges(container) => more_containers
+  def containers_for(node)
+    containers = @edges.select { |edge| edge.content == node }.map(&:container)
+
+    containers.each do |container|
+      containers += containers_for(container)
     end
 
-    all_containers
+    containers.uniq
+  end
+
+  def register_bag_and_contents(bag_text)
+    # First, separate the bag from its potential contents
+    container_color, contents = bag_text.split(' bags contain ')
+
+    # Either find the bag in the graph or, if not present yet, initialize it
+    container = find(container_color) || add(container_color)
+
+    # Use the regexp to scan for contents
+    contents = contents.scan(BAG_CONTENT_SCANNER).flatten
+    contents.each do |content|
+      # Parse out the bag's color and how many can be held by the container
+      amount = content.to_i
+      content_color = content.sub(/\d+ /, '')
+
+      # Either find the bag in the graph or, if not present yet, initialize it
+      content = find(content_color) || add(content_color)
+
+      # And, finally, create the edge representing how many of this bag the
+      # container can hold. Both the node and the graph will reference it.
+      edge = Edge.new(container: container, content: content, amount: amount)
+      container.edges << edge
+      @edges << edge
+    end
   end
 end
 
-class Bag
-  FORMAT = /(?:(\d+? ?\w+ \w+) bag)/
-
-  def self.parse(text)
-    text.scan(FORMAT).flatten
-  end
-end
-
-bag_graph = BagGraph.new
+graph = BagGraph.new
 
 input = File.expand_path('input.txt', File.dirname(__FILE__))
-File.read(input).split("\n").each do |line|
-  color, *others = Bag.parse(line)
-
-  bag_graph.register(color, others)
-end
+File.read(input).split("\n").each { |line| graph.register_bag_and_contents(line) }
 
 # Part One
-containers = bag_graph.containers_for("shiny gold").uniq
+shiny_gold_bag = graph.find("shiny gold")
+containers = graph.containers_for(shiny_gold_bag)
 
 puts "A total of #{containers.size} differently colored bags can contain a shiny gold bag directly or indirectly."
